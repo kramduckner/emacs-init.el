56c3becde6bf1469dccdd335509f1eef427d732b
Mark <buckner.a.mark@gmail.com>
2020-10-18 13:36:38 -0600

    added yasnippets, projectile, and some other congis

--- init.el
+++ init.el
@@ -35,12 +35,12 @@ There are two things you can do about this warning:
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(company-idle-delay 0.32)
+ '(company-idle-delay 0.2)
  '(custom-enabled-themes (quote (wombat)))
  '(js-indent-level 2)
  '(package-selected-packages
    (quote
-    (prettier-js helm lsp-ui company-lsp exec-path-from-shell flycheck lsp-python-ms dap-mode which-key ## company-tern xref-js2 js2-refactor js2-mode use-package lsp-mode)))
+    (react-snippets shr-tag-pre-highlight magit helm-projectile prettier-js helm lsp-ui company-lsp exec-path-from-shell flycheck lsp-python-ms dap-mode which-key ## company-tern xref-js2 js2-refactor js2-mode use-package lsp-mode)))
  '(show-paren-mode t))
 
 (custom-set-faces
@@ -51,54 +51,10 @@ There are two things you can do about this warning:
  '(lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic))))))
 
 
-;; (use-package lsp-mode
-;;   :commands (lsp lsp-deferred)
-;;   :hook (js-mode . lsp-deferred)	 
-;;   :config
-;; ;;  (add-hook 'js-mode-hook (lambda () (setq js-indent-level 2)))
-;; ;;  (add-hook 'js-mode-hook (lambda () (setq tab-width 2)))
-
-;;   (defun fmt-config-hooks ()
-;;     (add-hook 'before-save-hook #'lsp-format-buffer t t)
-;;     (add-hook 'before-save-hook #'lsp-organize-imports t t))
-;;   (add-hook 'prog-mode-hook #'fmt-config-hooks))
-
-;; optionally
-
-;; (use-package lsp-ui
-;;   :ensure t
-;;   :hook ((lsp-ui-mode)))
-
-;; (use-package lsp-ui
-;;   :after lsp-mode
-;;   :diminish
-;;   :commands lsp-ui-mode
-;;   :custom-face
-;;  ;; (lsp-ui-doc-background ((t (:background nil))))
-;;   (lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic)))))
-;;   :bind (:map lsp-ui-mode-map
-;;               ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
-;;               ([remap xref-find-references] . lsp-ui-peek-find-references)
-;;               ("C-c u" . lsp-ui-imenu))
-;;   :custom
-;;   (lsp-ui-doc-enable t)
-;;   (lsp-ui-doc-header t)
-;;   (lsp-ui-doc-include-signature t)
-;;   (lsp-ui-doc-position 'top)
-;;   (lsp-ui-doc-border (face-foreground 'default))
-;;   (lsp-ui-sideline-enable nil)
-;;   (lsp-ui-sideline-ignore-duplicate t)
-;;   (lsp-ui-sideline-show-code-actions nil)
-;;   :config
-;;   ;; Use lsp-ui-doc-webkit only in GUI
-;;   (setq lsp-ui-doc-use-webkit t)
-;;   ;; WORKAROUND Hide mode-line of the lsp-ui-imenu buffer
-;;   ;; https://github.com/emacs-lsp/lsp-ui/issues/243
-;;   (defadvice lsp-ui-imenu (after hide-lsp-ui-imenu-mode-line activate)
-;;     (setq mode-line-format nil)))
-
+;;here
 (use-package company-lsp :commands company-lsp)
 (add-hook 'after-init-hook 'global-company-mode)
+
 
 ;;;;;;;;;(add-hook 'after-init-hook 'js-mode)
 ;; if you are helm user
@@ -129,14 +85,6 @@ There are two things you can do about this warning:
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
-;;;; init.el ends here
-;; (add-hook 'js2-mode-hook
-;;           (defun my-js2-mode-setup ()
-;;             (flycheck-mode t)
-;;             (when (executable-find "eslint")
-;;               (flycheck-select-checker 'javascript-eslint))))
-
-
 ;; set transparency
 (set-frame-parameter (selected-frame) 'alpha '(90 90))
 (add-to-list 'default-frame-alist '(alpha 90 90))
@@ -148,8 +96,7 @@ There are two things you can do about this warning:
 (global-set-key (kbd "C-x m") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-buffers-persistent-kill)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-
-
+;;(global-set-key (kbd "<ret>") 'company-complete)
 
 (use-package prettier-js)
 (add-hook 'js-mode-hook 'prettier-js-mode)
@@ -158,3 +105,23 @@ There are two things you can do about this warning:
 ;;global-display-line-numbers-mode
  (add-hook 'after-init-hook #'global-display-line-numbers-mode)
 
+(require 'projectile)
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+(projectile-mode +1)
+
+
+
+;; remove this line of file searches take too long.
+(setq projectile-indexing-method 'hybrid)
+
+
+(require 'yasnippet)
+(yas-global-mode 1)
+
+
+(global-set-key (kbd "C-x g") 'magit-status)
+
+; Show suggestions after entering one character.
+(setq company-minimum-prefix-length 1)
+(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
